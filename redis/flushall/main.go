73d8ee33f282eb92f471e669d576de9d2fa7eecd
Mark Day <mark.day@aistemos.com>
2019-12-05 13:48:15 +0000

    tidy up

--- redis/flushall/main.go
+++ redis/flushall/main.go
@@ -3,6 +3,7 @@ package main
 // this is very destructive
 import (
 	"fmt"
+<<<<<<< Updated upstream
 	"github.com/go-redis/redis"
 	"os"
 	"time"
@@ -48,3 +49,31 @@ func manageRedisNode() {
 	//resp = rdb.FlushAll()
 	//fmt.Println(resp)
 }
+=======
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	isProd()
+}
+
+func isProd() {
+	url := "http://app.cipher.ai/version"
+	fmt.Printf("HTML code of %s ...\n", url)
+	resp, err := http.Get(url)
+	// handle the error if there is one
+	if err != nil {
+		panic(err)
+	}
+	// do this now so it won't be forgotten
+	defer resp.Body.Close()
+	// reads html as a slice of bytes
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	// show the HTML code as a string %s
+	fmt.Printf("%s\n", html)
+}
+>>>>>>> Stashed changes
